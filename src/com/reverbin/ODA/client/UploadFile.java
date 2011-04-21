b7a911fcc652a43b8304595dcc9a14cae819dfe7
anthony <5585976+freetink3r@users.noreply.github.com>
2011-04-21 01:26:11 +0000

    FileUpload dialog: added cancel button and icon
    git-svn-id: file:///subversion/oda/ODA@36 cfdba965-fe5d-466f-a9e7-793656018d40

--- src/com/reverbin/ODA/client/UploadFile.java
+++ src/com/reverbin/ODA/client/UploadFile.java
@@ -7,20 +7,24 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.FileUpload;
 import com.google.gwt.user.client.ui.FormPanel;
-import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteEvent;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.FormPanel.SubmitEvent;
 import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteHandler;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class UploadFile extends DialogBox {
 	
 	private static final String UPLOAD_ACTION_URL = GWT.getModuleBaseURL() + "upload";
 	private final Button buttonSubmit = new Button("Submit");
+	private final Button buttonCancel = new Button("Cancel");
 	
 	public UploadFile(SubmitCompleteHandler submitCompleteHandler) {
-		setHTML("Upload File");
+		
+		setHTML("<center>Upload File</center>");
+		Image img = new Image("images/upload.png");
 		
         // Create a FormPanel and point it at a service.
 		final FormPanel uploadForm = new FormPanel();
@@ -33,12 +37,22 @@ public class UploadFile extends DialogBox {
 		
 		// Create a panel to hold all of the form widgets.
 		VerticalPanel panel = new VerticalPanel();
+		panel.setSpacing(5);
 		uploadForm.setWidget(panel);
 		
 		// Create a FileUpload widget.
 		FileUpload fileUpload = new FileUpload();
 		fileUpload.setName("uploadFormElement");
-		panel.add(fileUpload);
+		
+		HorizontalPanel topRow = new HorizontalPanel();
+		topRow.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+		topRow.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
+		topRow.setWidth("100%");
+		topRow.add(img);
+		topRow.add(fileUpload);
+		topRow.setCellHorizontalAlignment(img, HasHorizontalAlignment.ALIGN_RIGHT);
+		topRow.setCellHorizontalAlignment(fileUpload, HasHorizontalAlignment.ALIGN_RIGHT);
+		panel.add(topRow);
 		
 		// Add a click handler to the submit button
 		buttonSubmit.addClickHandler(new ClickHandler() {
@@ -48,7 +62,24 @@ public class UploadFile extends DialogBox {
 			}
 		});
 		
-		panel.add(buttonSubmit);
+		// Add a click handler to the cancel button
+		buttonCancel.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				//UploadFile.this.buttonSubmit.setEnabled(false);
+				hide();
+			}
+		});
+		
+		HorizontalPanel hp = new HorizontalPanel();
+		hp.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+		hp.setWidth("100%");
+		hp.add(buttonSubmit);
+		hp.add(buttonCancel);
+		hp.setSpacing(2);
+		hp.setCellHorizontalAlignment(buttonSubmit, HasHorizontalAlignment.ALIGN_RIGHT);
+		hp.setCellHorizontalAlignment(buttonCancel, HasHorizontalAlignment.ALIGN_LEFT);
+
+		panel.add(hp);
 		
 		// Add an event handler to the form.
 		uploadForm.addSubmitHandler(new FormPanel.SubmitHandler() {
