6e18ecefe7d2efa53271e371fa592bcbec97cc00
davis <1972806+peoplesmeat@users.noreply.github.com>
2011-11-23 02:53:09 +0000

    git-svn-id: file:///subversion/oda/ODA/branches/jstl@85 cfdba965-fe5d-466f-a9e7-793656018d40

---
+++ src/main/java/com/onlinedisassembler/repository/Repository.java
@@ -0,0 +1,19 @@
+package com.onlinedisassembler.repository;
+
+import org.hibernate.Session;
+
+public class Repository<T,Tid> {
+	
+	Class<T> entityClass; 
+	Session session; 
+	public Repository(Class<T> entityClass) { 
+		this.entityClass = entityClass; 
+	}
+	
+	private synchronized Session getSession() { 
+		if (session==null) { 
+			session = OdaSessionFactory.getSessionFactory().getCurrentSession();
+		}
+		return session; 
+	}
+}
