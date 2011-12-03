39dad60340f76fa4a5f7362c6930358efc97bef1
davis <1972806+peoplesmeat@users.noreply.github.com>
2011-12-03 01:41:46 +0000

    moved this to test
    git-svn-id: file:///subversion/oda/ODA/branches/jstl@102 cfdba965-fe5d-466f-a9e7-793656018d40

---
+++ src/test/java/com/onlinedisassembler/web/WebServer.java
@@ -0,0 +1,30 @@
+package com.onlinedisassembler.web;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class WebServer {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		
+		System.setProperty("org.eclipse.jetty.util.log.DEBUG", "true");
+		Server server = new Server(8082);
+		WebAppContext context = new WebAppContext();
+		context.setResourceBase("src/main/webapp");		
+		context.setDescriptor("WEB-INF/web.xml");		
+		context.setContextPath("/");
+		context.setParentLoaderPriority(true); 
+		
+		
+		server.setHandler(context); 
+		
+		server.start(); 
+		server.join(); 
+
+	}
+
+}
